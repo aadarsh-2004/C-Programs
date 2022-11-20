0a16fbb87f1d6b9b5155c26a79372da2315b0358
aadarsh-2004 <84452674+aadarsh-2004@users.noreply.github.com>
2022-11-20 20:08:19 +0530

    Add files via upload

---
+++ (1) assignment/ASSIGNMENT-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	printf("hello");
+	printf("\nHELLO");
+	printf("\nHELLO WORLD");
+	printf("\nAADARSH ");
+	printf("\nSONI");
+	printf("\nAADARSH SONI");
+	printf("\nTECHNO INDIA NJR");
+	printf("\nTECHNO INDIA NJR");
+	printf("\nHELLO\n world");
+	printf("\nAADARSH\n SONI");
+}
+
